8dca887ce6b2fa9cde5a11187105b450f8d9e5c2
Pascal Sager <paes.sager@gmail.com>
2022-04-14 14:05:06 +0200

    Add logging

---
+++ src/utils/log.py
@@ -0,0 +1,22 @@
+import datetime
+import logging
+import sys
+from pathlib import Path
+
+
+def setup_logging():
+    """ Setup the logger """
+    logging.basicConfig(filename=Path('logs') / '{}.log'.format(datetime.datetime.now().strftime("%Y-%m-%d %H%M%S")),
+                        filemode="w", level=logging.DEBUG)
+
+    logger = logging.getLogger()  # the root logger
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setLevel(logging.INFO)
+    logger.addHandler(stream_handler)
+
+
+def format_logs(logs):
+    """ format logs in tqdm """
+    str_logs = ['{} - {:.4}'.format(k, v) for k, v in logs.items()]
+    s = ', '.join(str_logs)
+    return s
